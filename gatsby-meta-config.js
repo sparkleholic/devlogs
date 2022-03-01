62da1d6ee93c183711cca09c39a6ba18f0158795
Junil Kim <logyourself@gmail.com>
2022-03-02 00:22:35 +0900

    minor description changes

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `Dev logs and life logs`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  author: `[Junil]`,
+  introduction: `Dev logs in my daily life`,
+  siteUrl: `https://sparkleholic.github.io/devlogs`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: ``, // Your GitHub account
@@ -16,13 +16,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'sparkleholic/devlogs', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'sparkleholic',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
